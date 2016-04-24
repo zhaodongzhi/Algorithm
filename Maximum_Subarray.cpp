76b3c2960cdb77b7354f737fc71b6fce165c5125
zdz <1812844375a@gmail.com>
2016-04-24 14:38:55 +0800

    Update Maximum_Subarray.cpp

--- Maximum_Subarray.cpp
+++ Maximum_Subarray.cpp
@@ -6,21 +6,14 @@ public:
         }
         int result = 0x80000001;
         int tempresult = 0;
-        int maxelement = 0x80000001;
         for(int i = 0; i < nums.size(); ++i){
-            if(nums[i] > maxelement){
-                maxelement = nums[i];
-            }
-            if(tempresult < 0){
-                tempresult = 0;
-            }
             tempresult += nums[i];
             if(tempresult > result){
                 result = tempresult;
             }
-        }
-        if(maxelement < 0){
-            return maxelement;
+            if(tempresult < 0){
+                tempresult = 0;
+            }
         }
         return result;
     }
