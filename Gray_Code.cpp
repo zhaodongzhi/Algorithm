fd85d254703331642d0d21dd1ab7a1f91d14b092
zdz <1812844375a@gmail.com>
2016-08-14 23:18:47 +0800

    Create Gray_Code.cpp

---
+++ Gray_Code.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result(1, 0);
+        for(int i = 0; i < n; ++i){
+            for(int j = result.size()-1; j >= 0; --j){
+                result.push_back((result[j]^(1<<i)));
+            }
+        }
+        return result;
+    }
+};
